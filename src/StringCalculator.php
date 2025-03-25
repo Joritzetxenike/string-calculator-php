b44395dbb52a1ceb607570430510e2065482f426
Joritz <echenique.149324@e.unavarra.es>
2025-03-25 12:05:30 +0100

    [] - kata_tests_refactor

--- src/StringCalculator.php
+++ src/StringCalculator.php
@@ -2,7 +2,22 @@
 
 namespace Deg540\StringCalculatorPHP;
 
+use function PHPUnit\Framework\isEmpty;
+
 class StringCalculator
 {
-    // TODO: String Calculator Kata
+    public function Add(string $numbers): int
+    {
+        if(empty($numbers)) {
+            return 0;
+        }
+
+        if(strpos($numbers, ',')) {
+            $numbers = explode(',\n', $numbers);
+            return array_sum($numbers);
+
+        }
+
+        return $numbers;
+    }
 }
